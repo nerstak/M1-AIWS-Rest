e2098714d6e22be0a4f4f735929bcfd895bb10fb
nerstak <roy.karst@gmail.com>
2021-01-05 20:02:14 +0100

    Default 404 (not working)

---
+++ Rest-Server/src/main/java/rest/providers/NotFoundMapper.java
@@ -0,0 +1,23 @@
+package rest.providers;
+
+import rest.model.utils.AppException;
+
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import javax.ws.rs.core.MediaType;
+
+// Should be replacing 404, but doesn't seems to work
+
+@Provider
+public class NotFoundMapper implements ExceptionMapper<NotFoundException> {
+    @Override
+    public Response toResponse(NotFoundException ex) {
+        return Response
+                .status(Response.Status.NOT_FOUND)
+                .entity(new AppException(ex.getMessage(), Response.Status.NOT_FOUND))
+                .type(MediaType.APPLICATION_JSON_TYPE)
+                .build();
+    }
+}
