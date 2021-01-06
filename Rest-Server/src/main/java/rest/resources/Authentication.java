901564c3f540afc6c138843b56ff46eef4727101
Levasseur <sebastien.levasseur@efrei.net>
2021-01-06 22:41:02 +0100

    added json to auth

--- Rest-Server/src/main/java/rest/resources/Authentication.java
+++ Rest-Server/src/main/java/rest/resources/Authentication.java
@@ -1,16 +1,21 @@
 package rest.resources;
 
+
 import rest.dao.ManagerDAO;
 import rest.model.Manager;
 import rest.model.utils.AuthResponse;
 import rest.utils.JWTToken;
 
+import javax.json.*;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.*;
 import javax.xml.bind.JAXBElement;
+
+
+import java.io.StringReader;
 
 import static rest.utils.Constants.ERROR_CONNECTION_ERROR;
 import static rest.utils.Constants.ERROR_REQUEST_INCORRECT;
@@ -44,4 +49,39 @@ public class Authentication {
             return Response.status(Response.Status.UNAUTHORIZED).entity(res).build();
         }
     }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response authenticate(String jsonString) {
+        AuthResponse res = new AuthResponse();
+        try{
+            JsonReader jsonReader = Json.createReader(new StringReader(jsonString));
+            JsonObject jsonObject = jsonReader.readObject();
+            jsonReader.close();
+
+            String username =  jsonObject.getString("username");
+            String password = jsonObject.getString("password");
+            Manager mDB = managerDAO.selectUsername(username);
+
+            if(mDB == null) {
+                res.setError(ERROR_REQUEST_INCORRECT);
+                return Response.status(Response.Status.UNAUTHORIZED).entity(res).build();
+            }
+
+            if(mDB.getPassword().equals(password)) {
+                res.setToken(JWTToken.create(mDB.getIdManager(), mDB.getUsername()));
+                return Response.ok().entity(res).build();
+            } else {
+                res.setError(ERROR_CONNECTION_ERROR);
+                return Response.status(Response.Status.UNAUTHORIZED).entity(res).build();
+            }
+
+        } catch (Exception e) {
+            res.setError(ERROR_REQUEST_INCORRECT);
+            return Response.status(Response.Status.UNAUTHORIZED).entity(res).build();
+        }
+    }
+
+
 }
