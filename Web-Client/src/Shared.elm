6c0b713d2a8c9794aec070385399e5fb61c4bf7e
obloow <louismarie.merminod@gmail.com>
2021-01-07 10:34:26 +0100

    1rst pimp

--- Web-Client/src/Shared.elm
+++ Web-Client/src/Shared.elm
@@ -37,8 +37,9 @@ type alias Model =
 init : Flags -> Url -> Key -> ( Model, Cmd Msg )
 init flags url key =
     ( Model url key 
-        [ { url = Route.toString Route.Top, label = text "Homepage" }
-        , { url = Route.toString Route.NotFound, label = text "Not found" }
+        [ { url = Route.toString Route.Top, label =  text "Cities" }
+        , { url = Route.toString Route.NotFound, label = text "Movies" }
+        , { url = Route.toString Route.NotFound, label = text "Theaters" }
         ]
     , Cmd.none
     )
@@ -75,9 +76,20 @@ view :
 view { page, toMsg } model =
     { title = page.title
     , body =
-        [ column [ padding 20, spacing 20, height fill, width fill]
+        [ column [ padding 20, spacing 50, height fill, width fill]
             [ viewHeader
-            , column [ height fill, width fill, explain <| Debug.todo ] <| List.map (mapBodyLinks toMsg) model.body
+            , image
+                  [ centerX
+                  , centerY
+                  , width
+                        (fill
+                            |> maximum 200
+                        )
+                  ]
+                  { src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F000%2F660%2F143%2Foriginal%2Fvector-movie-theater-icons.jpg&f=1&nofb=1"
+                  , description = "logo"
+                  }
+            , column [ height fill, width fill, spacing 10 ] <| List.map (mapBodyLinks toMsg) model.body
             ]
         ]
     }
@@ -85,7 +97,7 @@ view { page, toMsg } model =
 viewHeader : Element msg
 viewHeader =
     row [ spacing 20 ]
-        [ viewHeaderLinks { url = Route.toString Route.Top, label = text "Homepage" }
+        [ viewHeaderLinks { url = Route.toString Route.Top, label = text "Home" }
         , viewHeaderLinks { url = Route.toString Route.NotFound, label = text "Not found" }
         ]
 
@@ -93,20 +105,45 @@ viewHeaderLinks : { url : String, label : Element msg } -> Element msg
 viewHeaderLinks =
     link
         [ padding 10
-        , Border.width 1
+        , Background.color menuColor
+        , Font.color textColor
+        ]
+
+contentLinks : { url : String, label : Element msg } -> Element msg
+contentLinks =
+    link
+        [ padding 10
         , Border.rounded 5
-        , mouseOver 
-            [ Background.color purple ]
+        , Background.color elementColor
+        , Font.color textColor
+        , Font.center
+        , centerX
+        , width
+            (fill
+                |> maximum 400
+            )
         ]
 
 mapBodyLinks : (Msg -> msg) -> { url : String, label : Element Msg } -> Element msg
 mapBodyLinks toMsg { url, label } =
-    viewHeaderLinks
+    contentLinks
         { url = url
-        , label = map toMsg label
+        , label =  map toMsg label
         }
 
 
-purple : Color
-purple =
-    rgb255 238 20 238
+backColor : Color
+backColor =
+    rgb255 11 19 43
+
+textColor : Color
+textColor =
+    rgb255 255 255 255
+
+elementColor : Color
+elementColor =
+    rgb255 58 80 107
+
+menuColor : Color
+menuColor =
+    rgb255 91 192 190
