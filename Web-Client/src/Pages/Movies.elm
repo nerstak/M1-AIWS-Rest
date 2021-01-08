2b37c0f61bd28d46ee82db68da9f4c18ee62df54
obloow <louismarie.merminod@gmail.com>
2021-01-08 13:11:48 +0100

    Add movies form

--- Web-Client/src/Pages/Movies.elm
+++ Web-Client/src/Pages/Movies.elm
@@ -2,6 +2,8 @@ module Pages.Movies exposing (Params, Model, Msg, page)
 
 import Colors
 import Element.Font as Font
+import Form exposing (Form)
+import Form.View
 import Shared
 import Spa.Document exposing (Document)
 import Spa.Page as Page exposing (Page)
@@ -10,7 +12,7 @@ import Http
 import RemoteData exposing (RemoteData(..), WebData)
 import Element exposing (..)
 import Spa.Generated.Route as Route
-import API
+import API exposing (Actor)
 
 
 page : Page Params Model Msg
@@ -35,6 +37,8 @@ type alias Params =
 
 type alias Model =
     { body : List UrlInfo
+    ,   inputValues : Form.View.Model InputValues
+    ,   token : Result String String
     }
 
 type alias UrlInfo =
@@ -44,7 +48,16 @@ type alias UrlInfo =
 
 init : Shared.Model -> Url Params -> ( Model, Cmd Msg )
 init shared { params } =
-    ( { body = [] }
+    ( { body = []
+     , inputValues = Form.View.idle
+                   { name = ""
+                   , pegi = ""
+                   , duration = ""
+                   , realisator = ""
+                   , actors = []
+                   }
+               , token = Err ""
+     }
     , API.getMovies GotMovies
     )
 
@@ -54,7 +67,10 @@ init shared { params } =
 
 
 type Msg
-    = GotMovies (WebData API.Movies)
+    = FormChanged (Form.View.Model InputValues)
+    | AddMovie OutputValues
+    | GotMovies (WebData API.Movies)
+    | GotToken (WebData String)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -62,6 +78,15 @@ update msg model =
     case msg of
         GotMovies data ->
             updateGotMovies data model
+
+        FormChanged newForm ->
+                    ( { model | inputValues = newForm }, Cmd.none )
+
+        AddMovie outputValues ->
+            ( model, API.postAuth outputValues.name outputValues.name GotToken )
+
+        GotToken data ->
+            updateGotToken data model
 
 updateGotMovies : WebData API.Movies -> Model -> ( Model, Cmd Msg )
 updateGotMovies data model =
@@ -72,10 +97,24 @@ updateGotMovies data model =
             updateFailure error model
         _ ->
             (model, Cmd.none)
+
+updateGotToken : WebData API.Token -> Model -> ( Model, Cmd Msg )
+updateGotToken data model =
+    case data of
+        Success token ->
+            updateSuccessToken token model
+        Failure error ->
+            updateFailure error model
+        _ ->
+            (model, Cmd.none)
         
 updateSuccess : API.Movies -> Model -> ( Model, Cmd Msg )
 updateSuccess movies model =
     ( { model | body = List.map movieToUrlInfo movies }, Cmd.none )
+
+updateSuccessToken : String -> Model -> ( Model, Cmd Msg )
+updateSuccessToken token model =
+    ( { model | token = Ok token }, Cmd.none )
 
 movieToUrlInfo : API.Movie -> UrlInfo
 movieToUrlInfo movie =
@@ -176,7 +215,134 @@ subscriptions model =
 view : Model -> Document Msg
 view model =
     { title = "Movies"
-    , body = []
+    , body =
+        [ el [ centerX ] <| html <| Form.View.asHtml
+            { onChange = FormChanged
+            , action = "Add"
+            , loading = "Adding..."
+            , validation = Form.View.ValidateOnSubmit
+            }
+            (Form.map AddMovie moviesForm) model.inputValues
+        , el [ centerX ] <| text <| Maybe.withDefault "" <| Result.toMaybe model.token
+        ]
     }
 
 
+-- FORM
+
+type alias InputValues =
+    { name : String
+    , pegi : String
+    , duration : String
+    , realisator : String
+    , actors : List Actor
+    }
+
+-- usefull if output values are different, here it's more architectural than utilitarian
+type alias OutputValues =
+    { name : String
+    , pegi : String
+    , duration : String
+    , realisator : String
+    , actors : List Actor
+    }
+
+moviesForm : Form InputValues OutputValues
+moviesForm =
+    Form.succeed OutputValues
+        |> Form.append nameField
+        |> Form.append pegiField
+        |> Form.append durationField
+        |> Form.append realisatorField
+        |> Form.append actorsForm
+
+
+nameField : Form { r | name : String } String
+nameField =
+    Form.textField
+        { parser = Ok
+        , value = .name
+        , update = \value values -> { values | name = value }
+        , error = always Nothing
+        , attributes =
+            { label = "Name"
+            , placeholder = "Name"
+            }
+        }
+
+pegiField : Form { r | pegi : String } String
+pegiField =
+    Form.textField
+        { parser = Ok
+        , value = .pegi
+        , update = \value values -> { values | pegi = value }
+        , error = always Nothing
+        , attributes =
+            { label = "Minimum age"
+            , placeholder = "Minimum age"
+            }
+        }
+
+durationField : Form { r | duration : String } String
+durationField =
+    Form.textField
+        { parser = Ok
+        , value = .duration
+        , update = \value values -> { values | duration = value }
+        , error = always Nothing
+        , attributes =
+            { label = "Duration"
+            , placeholder = "Duration"
+            }
+        }
+
+realisatorField : Form { r | realisator : String } String
+realisatorField =
+    Form.textField
+        { parser = Ok
+        , value = .realisator
+        , update = \value values -> { values | realisator = value }
+        , error = always Nothing
+        , attributes =
+            { label = "Realisator"
+            , placeholder = "Realisator"
+            }
+        }
+
+type alias Actor =
+    { name : String
+    }
+
+actorForm : Int -> Form Actor Actor
+actorForm id =
+    Form.succeed Actor
+            |> Form.append actorNameField
+
+actorsForm : Form { r | actors : List Actor } (List Actor)
+actorsForm =
+    Form.list
+        { default =
+            { name = ""
+            }
+        , value = .actors
+        , update = \value values -> { values | actors = value }
+        , attributes =
+            { label = "Actors"
+            , add = Just "Add actor"
+            , delete = Just "-"
+            }
+        }
+        actorForm
+
+actorNameField : Form { r | name : String } String
+actorNameField =
+    Form.textField
+        { parser = Ok
+        , value = .name
+        , update = \value values -> { values | name = value }
+        , error = always Nothing
+        , attributes =
+            { label = "Actor Name"
+            , placeholder = "Actor Name"
+            }
+        }
